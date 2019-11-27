4eeefa119944b6d6ac7906e32cde661edcae28c6
SkaZZ9 <eduardoamerico_@live.com>
2019-11-26 23:33:06 -0300

    Commit Final

--- js/scripts.js
+++ js/scripts.js
@@ -10,8 +10,11 @@ var numAcoes = 0;
 var parar = false;
 var colunaAtual = 0;
 var celula;
+var ultimo = 0;
 
 var teste = 0;
+
+var exemplo;
 
 
 function adcionarLinha(tabela) {
@@ -50,57 +53,71 @@ function pegarMatriz(tabela) {
     return matriz
 }
 
+
 function rodar(tabela) {
+
     parar = false
     inicializar(tabela);
     run();
 
 
 }
-function runPassoAPasso(){
+function startPassoAPasso(tabela) {
+
+    parar = false
+    inicializar(tabela);
+
+}
+function constinuaPassoAPasso() {
+    runPassoAPasso();
+}
+function runPassoAPasso() {
+
+    // console.log("fita")
+    // console.log(fita)
+    // console.log("posiçao na fita: " + posicao)
+    colunaAtual = null;
+    colunaAtual = pegarPosicaoSimboloAtual();
+    // console.log("posicao na coluna matriz: " + colunaAtual)
+    // console.log("posicao na linha matriz: " + estado)
+
+    if (colunaAtual == null || colunaAtual == undefined || colunaAtual == '' || colunaAtual == "") {
+        alert("elemento lido '" + fita[posicao] + "' não foi encontrado na lista de simbolos");
+        parar = true;
+        return false;
+    }
+    estado = parseInt(estado, 10);
+    celula = matriz[estado][colunaAtual];
+    if (celula == null || celula == undefined || celula == '' || celula == "") {
+        mostrarNaTelaFinal(fita);
+        alert("A Celula lida está vazia. O programa foi finalizado");
+        parar = true;
+        return false;
+    }
+    celula = celula.split(';', 3);
+
+    mostrarNaTela(fita, celula);
+
+    fazerAcaoCelula(celula)
+
+
+    numAcoes++;
+    if (maxAcoes == numAcoes) {
+        mostrarNaTelaFinal(fita);
+        alert("Chegou ao numero maximo de ações '" + maxAcoes + "'. O programa foi finalizado")
+        parar = true;
+        return false;
+    }
 
 }
 function run() {
-    
-    
+
+
 
     while (!parar == true) {
 
 
-        // console.log("fita")
-        // console.log(fita)
-        // console.log("posiçao na fita: " + posicao)
-        colunaAtual = null;
-        colunaAtual = pegarPosicaoSimboloAtual();
-        // console.log("posicao na coluna matriz: " + colunaAtual)
-        // console.log("posicao na linha matriz: " + estado)
-
-        if (colunaAtual == null || colunaAtual == undefined || colunaAtual == '' || colunaAtual == "") {
-            alert("elemento lido '" + fita[posicao] + "' não foi encontrado na lista de simbolos");
-            parar = true;
-            break;
-        }
-        celula = matriz[estado][colunaAtual];
-        if (celula == null || celula == undefined || celula == '' || celula == "") {
-            mostrarNaTelaFinal(fita);
-            alert("A Celula lida está vazia. O programa foi finalizado");
-            parar = true;
-            break;
-        }
-        celula = celula.split(';', 3);
-
-        mostrarNaTela(fita, celula);
-
-        fazerAcaoCelula(celula)
-
-
-        numAcoes++;
-        if (maxAcoes == numAcoes) {
-            mostrarNaTelaFinal(fita);
-            alert("Chegou ao numero maximo de ações '" + maxAcoes + "'. O programa foi finalizado")
-            parar = true;
-            break;
-        }
+        runPassoAPasso();
 
     }
 
@@ -129,38 +146,44 @@ function mostrarNaTela(fita, celula) {
 }
 
 function mostrarNaTelaFinal(fita) {
+    if (ultimo == 0) {
 
-    var resultado = document.getElementById('resultado')
-    var string = '';
-    var div = document.createElement("div");
+        var resultado = document.getElementById('resultado')
+        var string = '';
+        var div = document.createElement("div");
 
-    for (var i = 0; i < fita.length; i++) {
-        if (posicao == i) {
-            var aux = "{" + fita[posicao] + "}";
-            string = string + aux;
-        } else {
-            string = string + fita[i]
+        for (var i = 0; i < fita.length; i++) {
+            if (posicao == i) {
+                var aux = "{" + fita[posicao] + "}";
+                string = string + aux;
+            } else {
+                string = string + fita[i]
+            }
         }
+
+
+
+        div.innerHTML = "<p>Fita:&nbsp;" + string + "<br>Ação:&nbsp;" + numAcoes + " &nbsp;&nbsp;&nbsp;Estado Atual:&nbsp;" + estado + " &nbsp;&nbsp;&nbsp;Simbolo Atual:&nbsp;" + fita[posicao]
+            + "</p>";
+        resultado.appendChild(div);
     }
+    ultimo++;
 
 
 
-    div.innerHTML = "<p>Fita:&nbsp;" + string + "<br>Ação:&nbsp;" + numAcoes + " &nbsp;&nbsp;&nbsp;Estado Atual:&nbsp;" + estado + " &nbsp;&nbsp;&nbsp;Simbolo Atual:&nbsp;" + fita[posicao]
-        + "</p>";
-
-
-    resultado.appendChild(div);
 }
+
 
 
 function fazerAcaoCelula(celula) {
     estado = celula[0];
+    estado = parseInt(estado, 10);
 
 
     if (posicao == 0 && celula[1] != fita[posicao]) {
         fita = addInicio(fita);
         posicao++;
-    } else if (posicao == (fita.length - 1) && celula[2] == 'D') {
+    } else if (posicao == (fita.length - 1) && celula[1] != fita[posicao]) {
         fita = addFinal(fita);
     }
 
@@ -179,14 +202,15 @@ function inicializar(tabela) {
     matriz = []; //padrão []
     estado = 1; //padrão 1
     algoritmo = document.getElementById('algoritmo').value
-    
+    maxAcoes = document.getElementById('acoes').value
+
     fita = algoritmo;
     fita = transformarFita(fita);
-    
+
     posicao = 1;
     celula = null;
-    maxAcoes = 150;
     numAcoes = 0;
+    ultimo = 0;
 
     matriz = pegarMatriz(tabela);
 
@@ -195,7 +219,7 @@ function inicializar(tabela) {
     document.getElementById('resultado').textContent = '';
 
 
-    
+
 }
 
 function transformarFita(fita) {
@@ -237,15 +261,47 @@ function pegarPosicaoSimboloAtual() {
 
 
 
-// function initiateArrays(value) {
-//     var values = []
-//     listRight = undefined
-//     listLeft = []
-//     values = Array.from(value)
-//     //o programa sempre inicia com o simbolo inicial (>) na posicao zero
-//     if (values[0] != '>') {
-//         alert('Voce precisa iniciar com o simbolo inicial!')
-//     } else {
-//         listRight = values
-//     }
-// }
+function escreverImput(idImput, valor) {
+    document.getElementById(idImput).value = valor;
+}
+
+function escreverExemplo(exemplo) {
+    document.getElementById('tadelaDeAcao').textContent = '';
+    var aux = document.createElement("tbody");
+    var tabela = document.getElementById('tadelaDeAcao');
+
+    aux.innerHTML = exemplo;
+    tabela.appendChild(aux);
+
+}
+function exemploSoma() {
+    exemplo = "<tr> <td>#</td><td>*</td><td>_</td><td>&gt;</td></tr><tr><td>1</td><td contenteditable='true'></td><td contenteditable='true'></td><td contenteditable='true'>2;&gt;;D</td></tr><tr><td contenteditable='true'>2</td><td contenteditable='true'>2;*;D</td><td contenteditable='true'>3;_;D</td><td contenteditable='true'></td></tr><tr><td contenteditable='true'>3</td><td contenteditable='true'><span style='background-color: rgba(0, 0, 0, 0.05);'>3;*;D</span></td><td contenteditable='true'>4;_;E</td><td contenteditable='true'></td></tr><tr><td contenteditable='true'>4</td><td contenteditable='true'><span style='background-color: rgb(255, 255, 255);'>5;_;E</span><br></td><td contenteditable='true'></td><td contenteditable='true'></td></tr><tr><td contenteditable='true'>5</td><td contenteditable='true'>5;*;E</td><td contenteditable='true'>6;*;D</td><td contenteditable='true'></td></tr><tr><td contenteditable='true'>6</td><td contenteditable='true'></td><td contenteditable='true'></td><td contenteditable='true'></td></tr>";
+    escreverExemplo(exemplo);
+    escreverImput("acoes", 100);
+    escreverImput("algoritmo", ">***_***");
+}
+function exemploSub() {
+    exemplo = "<tr> <td>#</td><td>*</td><td>_</td><td>&gt;</td></tr><tr><td>1</td><td contenteditable='true'></td><td contenteditable='true'></td><td contenteditable='true'>2;&gt;;D</td></tr><tr><td contenteditable='true'>2</td><td contenteditable='true'>2;*;D</td><td contenteditable='true'>3;_;D</td><td contenteditable='true'></td></tr><tr><td contenteditable='true'>3</td><td contenteditable='true'><span style='background-color: rgba(0, 0, 0, 0.05);'>3;*;D</span></td><td contenteditable='true'>4;_;E</td><td contenteditable='true'></td></tr><tr><td contenteditable='true'>4</td><td contenteditable='true'><span style='background-color: rgb(255, 255, 255);'>5;_;E</span><br></td><td contenteditable='true'></td><td contenteditable='true'></td></tr><tr><td contenteditable='true'>5</td><td contenteditable='true'>5;*;E</td><td contenteditable='true'>6;_;E</td><td contenteditable='true'></td></tr><tr><td contenteditable='true'>6</td><td contenteditable='true'>6;*;E</td><td contenteditable='true'></td><td contenteditable='true'>7;_;D</td></tr><tr><td contenteditable='true'>7</td><td contenteditable='true'>2;&gt;;D</td><td contenteditable='true'></td><td contenteditable='true'></td></tr>"
+    escreverExemplo(exemplo);
+    escreverImput("acoes", 500);
+    escreverImput("algoritmo", ">***_**");
+}
+function exemploFrente() {
+    exemplo = "<tr><td>#</td><td>*</td><td>_</td><td>&gt;</td></tr><tr><td>1</td><td contenteditable='true'>1;A;D</td><td contenteditable='true'>1;A;D</td><td contenteditable='true'>1;&gt;;D</td></tr>";
+    escreverExemplo(exemplo);
+    escreverImput("acoes", 50);
+    escreverImput("algoritmo", ">***");
+}
+function exemploAtras() {
+    exemplo = "<tr><td>#</td><td>*</td><td>_</td><td>&gt;</td></tr><tr><td>1</td><td contenteditable='true'>1;A;E</td><td contenteditable='true'>1;A;E</td><td contenteditable='true'>1;&gt;;E</td></tr>";
+    escreverExemplo(exemplo);
+    escreverImput("acoes", 50);
+    escreverImput("algoritmo", ">***");
+}
+function exemploVaiEVolta() {
+    exemplo = "<tr><td>#</td><td>*</td><td>_</td><td>&gt;</td><td contenteditable='true'>A</td><td contenteditable='true'></td></tr><tr><td>1</td><td contenteditable='true'>1;A;D</td><td contenteditable='true'>2;*;E</td><td contenteditable='true'>1;&gt;;D</td><td contenteditable='true'>1;A;D</td><td contenteditable='true'></td></tr><tr><td>2 </td><td contenteditable='true'></td><td contenteditable='true'></td><td contenteditable='true'>3;*;E</td><td contenteditable='true'>2;*;E</td><td contenteditable='true'></td></tr><tr><td contenteditable='true'>3</td><td contenteditable='true'></td><td contenteditable='true'>4;&gt;;D</td><td contenteditable='true'><br></td><td contenteditable='true'></td><td contenteditable='true'></td></tr><tr><td contenteditable='true'>4</td><td contenteditable='true'></td><td contenteditable='true'></td><td contenteditable='true'></td><td contenteditable='true'></td><td contenteditable='true'></td></tr>";
+    escreverExemplo(exemplo);
+    escreverImput("acoes", 100);
+    escreverImput("algoritmo", ">****");
+}
+
